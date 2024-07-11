b7f97aa00067a759eb777fed40ecb783e069cf98
vearsds <andreasgustaviano@gmail.com>
2024-07-11 16:57:45 +0700

    new design

--- src/App.jsx
+++ src/App.jsx
@@ -31,24 +31,31 @@ const App = () => {
     // return `${days} DAYS : ${hours} HOURS : ${minutes} MINUTES : ${seconds} SECONDS`;
   };
   return (
-    <main className='flex flex-col bg-space-pattern h-[100vh]'>
-      <div className='flex items-center flex-1 w-full justify-center'>
-        <img src='/imgs/bxfull_rectangle_white 1.png'></img>
-      </div>
-      <div className='flex flex-col bg-service-section bg-cover md:bg-transparent items-center justify-center w-full h-[70%] b-0 md:gap-10 bg-black'>
-        <label className=' text-[15px] md:text-[26px] font-normal leading-[41px]'>We are on the way back from the future</label>
-        <label className='text-[42px] md:text-[80px] font-bold leading-[42px]'>ARRIVING SOON IN</label>
-        <div className='flex text-[42px] md:text-[80px] font-bold leading-[90px] gap-2 '>
-          <label className=''>{getFormattedTime(time).days}:</label>
-          <label>{getFormattedTime(time).hours}:</label>
-          <label>{getFormattedTime(time).minutes}:</label>
-          <label>{getFormattedTime(time).seconds}</label>
-          <label>HRS</label>
+    <main className='bg-space-pattern bg-cover h-[100vh]'>
+      <div className=' fixed w-full -z-10 bg-gradient-to-b from-black to-[#070709] h-[30vh]'></div>
+      <div className='flex flex-col h-[100vh]'>
+        <div className='flex items-center flex-1 justify-center'>
+          <img src='/imgs/bxfull_rectangle_white 1.png'></img>
         </div>
-        <div className='flex items-center flex-1 gap-1 '>
-          <img src='/imgs/instagram.png' />
-          <a href='https://www.instagram.com/bastian.produx?igsh=MTkxcXpteTA5YWF1'>Our Instagram</a>
-          <ChevronIcon />
+        <div className='flex flex-col bg-service-section bg-cover md:bg-transparent items-center justify-start md:justify-center w-full h-[70%] b-0 md:gap-10 bg-black'>
+          <label className=' text-[15px] md:text-[26px] font-normal leading-[41px]'>We are on the way back from the future</label>
+          <label className='title-homescreen'>ARRIVING SOON IN</label>
+          <div className='flex gap-2 title-homescreen '>
+            <label className=''>{getFormattedTime(time).days}:</label>
+            <label>{getFormattedTime(time).hours}:</label>
+            <label>{getFormattedTime(time).minutes}:</label>
+            <label>{getFormattedTime(time).seconds}</label>
+            <label>HRS</label>
+          </div>
+          <div className='flex items-start flex-1 gap-1 flex-col m-20 md:items-center md:flex-row'>
+            <img src='/imgs/instagram.png' />
+            <div className='flex'>
+              <a href='https://www.instagram.com/bastian.produx?igsh=MTkxcXpteTA5YWF1'>Our Instagram</a>
+              <span>
+                <ChevronIcon />
+              </span>
+            </div>
+          </div>
         </div>
       </div>
     </main>
